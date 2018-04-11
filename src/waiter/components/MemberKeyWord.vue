bb1e45ef40da776a5c9d9a0e3fb50dae39eed97d
STM32King <30915620+STM32King@users.noreply.github.com>
2018-04-11 10:04:51 +0800

    MemberKeyWord -- Sbmit

--- src/waiter/components/MemberKeyWord.vue
+++ src/waiter/components/MemberKeyWord.vue
@@ -1,6 +1,5 @@
 <template>
  <div>
-   <h1>{{testProps}}</h1>
    <el-button type="text" @click="dialogVisible = true">添加会员</el-button>
   <!-- 会员关键字窗口 -> START -->
   <el-dialog  title="会员关键字"  :visible.sync="dialogVisible"  width="30%"  :before-close="handleClose">
@@ -18,12 +17,6 @@
 
 <script>
 export default {
-  // props: {
-  //     testProps: {
-  //       type: String,
-  //       required: true
-  //     },
-  //   },
     data() {
       return {
         dialogVisible: false, //是否显示窗口标志位
