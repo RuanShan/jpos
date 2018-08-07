d601fef022118f967d867e310de053383dfe6b0c
qinghe <david@getstore.cn>
2018-08-07 14:42:37 +0800

    修正标签打印脚本

--- src/ipc/labelPrint.js
+++ src/ipc/labelPrint.js
@@ -7,7 +7,7 @@ export function printLabel ( ){
   const encoding="gb2312"
   // Select the adapter based on your printer type
   // {title, code, memo}
-  let path = __dirname + '/templates/20x60_barcode_label.tspl'
+  let path = __dirname + '/templates/20x50_barcode_label.tspl'
   console.log('path', path)
 
   fs.readFile(path,'ascii', function(err,data){
@@ -24,7 +24,7 @@ export function printLabel ( ){
      //Append linebreak (\r) to all commands
      data = data + "\r" //Use double quotes to interpolate
      var compiled = _.template( data );
-     data = iconv.encode( compiled({ 'label_title': '什么有用中文'}), encoding);
+     data = iconv.encode( compiled({ 'label_title': '永峰皮具养护物品条码', 'group_number': '0614010001', 'item_memos': '1这是多行工作注释内容 2这是多行工作注释,内容3这是多行工作注释内容，约30字'}), encoding);
 
      console.log("raw data", data, printer)
      if( printer ){
