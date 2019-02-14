38249f07b08fa9075f8df01f65eab5611d2c0527
qinghe <david@getstore.cn>
2019-02-14 14:29:49 +0800

    doing  order delivery on pad

--- src/waiter/pad/OrderDelivery.vue
+++ src/waiter/pad/OrderDelivery.vue
@@ -16,6 +16,77 @@
       vertical-align: middle;
     }
   }
+  .main-content{
+    padding-top: 60px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+  .customer-container {
+    .search-result{
+      margin-bottom: 10px;
+      background-color: #fff;
+    }
+    .search-form {
+        .el-form-item {
+            margin-bottom: 5px;
+        }
+        .search-input{
+          width: 18em;
+        }
+    }
+    table {
+        td {
+            max-width: 20%;
+        }
+        th{ width: 9em;}
+        td,
+        th {
+            border: 1px solid #ebeef5;
+            padding: 6px 10px;
+            font-size: 14px;
+            box-sizing: border-box;
+            white-space: normal;
+            line-height: 23px;
+        }
+        th span{ font-weight: bold;}
+    }
+  }
+
+  .order-item-list {
+      position: absolute;
+      top: 190px;
+      bottom: 140px;
+      left: 10px;
+      right: 10px;
+  }
+  .order-final {
+      position: absolute;
+      bottom: 60px;
+      left: 10px;
+      right: 10px;
+      .check-button {
+          padding: 0;
+          width: 100%;
+          line-height: 50px;
+          height: 50px;
+          text-align: center;
+          font-size: 21px;
+          background-color: #67c23a;
+          border-color: #67c23a;
+          span {
+              color: #fff;
+          }
+      }
+      .order-sum {
+        line-height: 30px;
+        height: 30px;
+        text-align: center;
+        margin: 0 10px;
+        i {
+              font-size: 12px;
+          }
+      }
+  }
 }
 </style>
 
@@ -25,88 +96,89 @@
   <div class="order-delivery-container">
     <head-top></head-top>
 
+
     <!-- 结账组件 Start-->
     <CheckoutDialog :order-item-list="checkoutRequiredLineItems" :totalMoney="totalMoney" :customer="currentCustomer"  :card="currentCard"  :dialog-visible.sync="checkoutDialogVisible" @payment-created-event="handlePaymentCreated"></CheckoutDialog>
     <!-- 结账组件 End-->
+    <div class="main-content">
+      <div class="customer-container clear">
+        <el-form ref="customerForm"    :inline="true" class="search-form">
+          <el-form-item label="客户搜索">
+            <el-select v-model="customerComboId" :remote-method="searchCustomers" :default-first-option="true"
+            placeholder="请输入手机号/姓名/会员卡号" filterable remote clearable @change="handleCustomerChanged" @clear="handleCustomerChanged">
+              <el-option v-for="item in computedCustomerOptions" :key="item.value" :label="item.label" :value="item.value">
+              </el-option>
+            </el-select>
+          </el-form-item>
 
-    <div class="customer-container clear">
-      <el-form ref="customerForm" size="mini"  :inline="true" class="search-form">
-        <el-form-item label="客户搜索">
-          <el-select v-model="customerComboId" :remote-method="searchCustomers" :default-first-option="true"
-          placeholder="请输入手机号/姓名/会员卡号" filterable remote clearable @change="handleCustomerChanged" @clear="handleCustomerChanged">
-            <el-option v-for="item in computedCustomerOptions" :key="item.value" :label="item.label" :value="item.value">
-            </el-option>
-          </el-select>
-        </el-form-item>
+          <el-form-item label="订单搜索">
+            <el-select v-model="orderComboId" :remote-method="searchOrders" placeholder="请输入订单/物品号" filterable remote clearable @change="handleOrderChanged" @clear="handleOrderChanged">
+              <el-option v-for="item in computedOrderOptions" :key="item.value" :label="item.label" :value="item.value">
+              </el-option>
+            </el-select>
+          </el-form-item>
 
-        <el-form-item label="订单搜索">
-          <el-select v-model="orderComboId" :remote-method="searchOrders" placeholder="请输入订单/物品号" filterable remote clearable @change="handleOrderChanged" @clear="handleOrderChanged">
-            <el-option v-for="item in computedOrderOptions" :key="item.value" :label="item.label" :value="item.value">
-            </el-option>
-          </el-select>
-        </el-form-item>
-
-      </el-form>
-      <div class="search-result clear">
-        <div >
-          <table style="width:100%">
-            <tr><th>客户类型</th><td>{{currentCustomer.customerType}}</td><th>移动电话</th><td>{{currentCustomer.mobile}}</td>
-              <th>消费金额</th>
-              <td> <span v-show="currentCustomer.normalOrderTotal">¥</span> {{currentCustomer.normalOrderTotal}}</td>
-            </tr>
-            <tr ><th>会员卡号</th><td>{{currentCard.code}}</td>
-              <th>会员卡类型</th>
-              <td><span class="ellipsis card-type">{{currentCard.name}}</span></td>
-              <th>会员卡余额</th>
-              <td><span v-show="currentCard.style=='times'">{{currentCard.cardTimesRemaining}}次</span><span v-show="currentCard.style=='prepaid'">¥ {{currentCard.amountRemaining}}</span></td>
-            </tr>
-          </table>
+        </el-form>
+        <div class="search-result clear">
+          <div >
+            <table style="width:100%">
+              <tr><th>客户类型</th><td>{{currentCustomer.customerType}}</td><th>移动电话</th><td>{{currentCustomer.mobile}}</td>
+                <th>消费金额</th>
+                <td> <span v-show="currentCustomer.normalOrderTotal">¥</span> {{currentCustomer.normalOrderTotal}}</td>
+              </tr>
+              <tr ><th>会员卡号</th><td>{{currentCard.code}}</td>
+                <th>会员卡类型</th>
+                <td><span class="ellipsis card-type">{{currentCard.name}}</span></td>
+                <th>会员卡余额</th>
+                <td><span v-show="currentCard.style=='times'">{{currentCard.cardTimesRemaining}}次</span><span v-show="currentCard.style=='prepaid'">¥ {{currentCard.amountRemaining}}</span></td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="order-item-list">
+        <el-table :data="computedLineItemGroups" border style="width:100%;" class="fillcontain cel-scrollable-table" highlight-current-row  @current-change="handleCurrentGroupChange">
+          <el-table-column prop="displayCreatedAt" label="订单日期" align="center" width="120" >
+            <template slot-scope="scope">
+              <div>{{scope.row.createdAt.format("YYYY-MM-DD")}}</div>
+                <div>{{scope.row.createdAt.format("H:mm")}}</div>
+            </template>
+          </el-table-column>
+          <el-table-column label="物品条码" width="120" align="center">
+            <template slot-scope="scope">
+              <div class="image-wrap">
+                <img :src="scope.row.imageUrl" alt="">
+              </div>
+              <div class="group-number">{{scope.row.number}} </div>
+            </template>
+          </el-table-column>
+          <el-table-column prop="name" label="物品-服务项目[备注]">
+            <template slot-scope="scope">
+              <div v-for="item in scope.row.lineItems">{{item.cname}}<span v-show="item.memo">[{{item.memo}}] </span></div>
+            </template>
+          </el-table-column>
+          <el-table-column prop="displayState" label="物品状态" width="80" ></el-table-column>
+          <el-table-column prop="order.displayPaymentState" label="支付状态" width="80" ></el-table-column>
+          <el-table-column prop="price" label="金额" width="60">金额</el-table-column>
+          <el-table-column label="操作" width="50">
+            <template slot-scope="scope">
+              <el-button type="danger" icon="el-icon-delete"  circle @click="discardLineItemGroup(scope.row)" size="mini"></el-button>
+           </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div class="order-final">
+        <div class="order-sum">
+          <i>数量：</i>
+          <span>{{totalCount}}</span>&nbsp;&nbsp;&nbsp;
+          <i>金额：</i>
+          <span>￥{{totalMoney}}</span>&nbsp;
+        </div>
+        <div class=" font-color"  >
+          <el-button type="success" @click="handleDeliverOrders()" class="check-button">确认取单 ：&nbsp;￥{{totalMoney}}</el-button>
         </div>
       </div>
     </div>
-    <div class="order-item-list">
-      <el-table :data="computedLineItemGroups" border stripe style="width:100%;" class="fillcontain cel-scrollable-table" highlight-current-row  @current-change="handleCurrentGroupChange">
-        <el-table-column prop="displayCreatedAt" label="订单日期" align="center" width="120" >
-          <template slot-scope="scope">
-            <div>{{scope.row.createdAt.format("YYYY-MM-DD")}}</div>
-              <div>{{scope.row.createdAt.format("H:mm")}}</div>
-          </template>
-        </el-table-column>
-        <el-table-column label="物品条码" width="120" align="center">
-          <template slot-scope="scope">
-            <div class="image-wrap">
-              <img :src="scope.row.imageUrl" alt="">
-            </div>
-            <div class="group-number">{{scope.row.number}} </div>
-          </template>
-        </el-table-column>
-        <el-table-column prop="name" label="物品-服务项目[备注]">
-          <template slot-scope="scope">
-            <div v-for="item in scope.row.lineItems">{{item.cname}}<span v-show="item.memo">[{{item.memo}}] </span></div>
-          </template>
-        </el-table-column>
-        <el-table-column prop="displayState" label="物品状态" width="80" ></el-table-column>
-        <el-table-column prop="order.displayPaymentState" label="支付状态" width="80" ></el-table-column>
-        <el-table-column prop="price" label="金额" width="60">金额</el-table-column>
-        <el-table-column label="操作" width="50">
-          <template slot-scope="scope">
-            <el-button type="danger" icon="el-icon-delete"  circle @click="discardLineItemGroup(scope.row)" size="mini"></el-button>
-         </template>
-        </el-table-column>
-      </el-table>
-    </div>
-    <div class="order-final">
-      <div class="order-sum">
-        <i>数量：</i>
-        <span>{{totalCount}}</span>&nbsp;&nbsp;&nbsp;
-        <i>金额：</i>
-        <span>￥{{totalMoney}}</span>&nbsp;
-      </div>
-      <div class=" font-color"  >
-        <el-button type="success" @click="handleDeliverOrders()" class="check-button">确认取单 ：&nbsp;￥{{totalMoney}}</el-button>
-      </div>
-    </div>
-
     <footer-bar></footer-bar>
 
   </div>
