0d6e4de691f5d7e3d8c8191e16d13218eedb08cd
qinghe <david@getstore.cn>
2019-06-26 15:58:38 +0800

    添加退出确认功能

--- src/waiter/page/home.vue
+++ src/waiter/page/home.vue
@@ -28,15 +28,6 @@ export default {
   },
   created() {
     this.validateCurrentUser()
-    window.onbeforeunload = (e) => {
-      console.log('I do not want to be closed')
-
-      // 与通常的浏览器不同,会提示给用户一个消息框,
-      //返回非空值将默认取消关闭
-      //建议使用对话框 API 让用户确认关闭应用程序.
-
-      e.returnValue = "close it?" // 相当于 `return false` ，但是不推荐使用
-    }
   },
   methods: {
     validateCurrentUser() {
